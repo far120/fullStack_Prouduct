d835cd2d5f76ca417a22ccd8ab1297cfc2a78fd4
far120 <tatafar2018@gmail.com>
2024-10-09 18:19:45 +0300

    v3

--- authentication-react/src/pages/website pages/validation/links.js
+++ authentication-react/src/pages/website pages/validation/links.js
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Mycontext } from "../../authentication/regester & login/context";
-
+import {BackEnd_url}  from '../../../constance';
 export default function Links() {
     const { params, setparams } = useContext(Mycontext);
     const location = useLocation();
@@ -9,7 +9,7 @@ export default function Links() {
     const category = path.split("/")[1];
 
     useEffect(() => {
-        fetch("http://localhost:2004/api/category")
+        fetch(`${BackEnd_url}/api/category`)
             .then(res => res.json())
             .then(data => setparams(data.filter(item => item.name === category)))
             .catch(err => console.error("Error fetching categories:", err));
