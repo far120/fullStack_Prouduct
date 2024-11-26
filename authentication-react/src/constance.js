d5af1fd13cc296afae35cb47851f76ad6b19358e
far120 <tatafar2018@gmail.com>
2024-11-26 12:07:45 +0200

    v5

--- authentication-react/src/constance.js
+++ authentication-react/src/constance.js
@@ -1,3 +1,4 @@
 
-export  const BackEnd_url = 'https://full-stack-prouduct.vercel.app';
+// export  const BackEnd_url = 'https://full-stack-prouduct.vercel.app'; // متعملش commit علشان كده انتا شغال التنين 
+ export  const BackEnd_url = 'https://prouduct-backend.vercel.app'; // متعملش commit علشان كده انتا شغال التنين 
 // export  const BackEnd_url = 'http://localhost:2004';
